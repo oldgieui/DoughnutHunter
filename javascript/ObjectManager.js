95c4eeeac3bb2d97ecac1f855ffa3fc4518bae44
이건희 <flecher1@gmail.com>
2014-09-22 14:48:17 +0900

    스프라이트 변경 및 추

--- javascript/ObjectManager.js
+++ javascript/ObjectManager.js
@@ -50,16 +50,16 @@ var ObjectManager = {
 		var pc = new this.GameObject({
 			"type" : "character",
 			"id" : "pc",
-			"width" : Utility.ScreenWidth * 0.04,
-			"height" : Utility.ScreenHeight * 0.12,
+			"width" : Utility.ScreenWidth * 0.05,
+			"height" : Utility.ScreenHeight * 0.07,
 			"posX" : (Utility.ScreenWidth * 0.3),
-			"posY" : (Utility.ScreenHeight * 0.75),
+			"posY" : (Utility.ScreenHeight * 0.85),
 			"vecX" : 0,
 			"vecY" : 0,
 			"accelX" : 0,
-			"accelY" : -9.8,
+			"accelY" : 0.5,
+			"SpriteSrc" : "image/megaman.png",
 			"isJumping" : false,
-			"SpriteSrc" : "image/STAND_R_00.png",
 			"update" : function(dTime){
 				var keyState = ControlManager.KeyMap;
 				if (keyState.left === true && keyState.right === false) {
@@ -71,15 +71,32 @@ var ObjectManager = {
 				else{
 					this.vecX = 0;
 				}
+
+				if (keyState.spacebar === true && this.isJumping === false) {
+					this.vecY = -10;
+					this.isJumping = true;
+				}
+
+
 		 		// console.log(this.vecX);
 		 		// if (keyState.spacebar === true && this.isJumping === false) {
 		 		// 	this.isJumping = true;
 		 		// 	//점프 관련은 나중에 추가하는 걸로
 		 		// }
 		 		//가속도가 어떻게 적용되는지 다시 알아볼것
-		 		var rightEnd = Utility.ScreenWidth - this.width;
-		 		if ((this.posX <= rightEnd && this.posX >= 0) || (this.posX >= rightEnd && this.vecX < 0) || (this.posX <= 0 && this.vecX > 0)) {
+		 		var xEnd = Utility.ScreenWidth - this.width;
+		 		var yEnd = Utility.ScreenHeight - this.height * 2;
+		 		if ((this.posX <= xEnd && this.posX >= 0) || (this.posX >= xEnd && this.vecX < 0) || (this.posX <= 0 && this.vecX > 0)) {
 		 			this.posX = this.posX + ((this.vecX + (this.vecX * this.accelX)) * dTime);
+		 		}
+		 		if (this.posY <= yEnd && this.posY >= 0) {
+		 			this.posY = this.posY + this.vecY;
+		 			this.vecY = this.vecY + this.accelY;
+		 		}
+		 		else if (this.posY > yEnd) {
+		 			this.posY = yEnd ;
+		 			this.vecY = 0;
+		 			this.isJumping = false;
 		 		}
 		 	},
 		 	"render" : function(){
@@ -102,7 +119,7 @@ var ObjectManager = {
 				"SpriteSrc" : "image/ball.jpg",
 				"update" : function(dTime){
 					var xEnd = Utility.ScreenWidth - this.width;
-					var yEnd = Utility.ScreenHeight - this.height;
+					var yEnd = Utility.ScreenHeight - this.height * 2;
 					if (this.posX <= xEnd && this.posX >= 0) {
 						this.posX = this.posX + this.vecX;
 						this.vecX = this.vecX + this.accelX;
@@ -147,23 +164,7 @@ var ObjectManager = {
 		var ball2 = new this.GameObject(ball(Utility.ScreenWidth * 0.25 - Utility.ScreenWidth * 0.25 % 1, Utility.ScreenHeight * 0.45 - Utility.ScreenHeight * 0.45 % 1));
 		var ball3 = new this.GameObject(ball(Utility.ScreenWidth * 0.5 - Utility.ScreenWidth * 0.5 % 1, Utility.ScreenHeight * 0.1 - Utility.ScreenHeight * 0.1 % 1));
 		var ball4 = new this.GameObject(ball(Utility.ScreenWidth * 0.9 - Utility.ScreenWidth * 0.9 % 1, Utility.ScreenHeight * 0.6 - Utility.ScreenHeight * 0.6 % 1));
-
-		// pc.setRender(function(){}):
-		// pc.setRender(function(){}):
-		// pc.on("update",function(){
-		// 	A.render();
-		// });
-		// pc.off();
-
-
-
-		// A.setGameObject(pc);
-		// A.some = function(){
-		// 	this.pc.on("update");
-		// 	this.setRender(fp);
-		// }
-
-
+		// var ball5 = new this.GameObject(ball(Utility.ScreenWidth * 0.1 - Utility.ScreenWidth * 0.1 % 1, Utility.ScreenHeight * 0.85 - Utility.ScreenHeight * 0.85 % 1));
 	},
 };
 
